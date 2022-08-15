738e5db46a83b64275b1867bd5582048e97c69fd
batshoes <james.middlemiss@valiant.finance>
2022-08-15 10:44:35 +1000

    Initialize Internet Speedtest + Notion Push

---
+++ index.js
@@ -0,0 +1,58 @@
+// Notion API Client from https://developers.notion.com/docs
+import { Client } from "@notionhq/client"
+// Import from from https://github.com/branchard/fast-speedtest-api
+import fastDotComAPI from 'fast-speedtest-api';
+
+// Initialize constants.
+const notion = new Client({ auth: process.env.NOTION_KEY })
+const databaseId = process.env.NOTION_DATABASE_ID
+const { UNITS } = fastDotComAPI;
+
+async function addItem(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: { 
+          title:[
+            {
+              "text": {
+                "content": text
+              }
+            }
+          ]
+        }
+      },
+    })
+    // Remove for a quieter console.
+    console.log(response)
+    console.log("Success! Entry added.")
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+// Initialize speedtest with API Key
+let speedtest = new fastDotComAPI({
+    token: process.env.FAST_DOT_COM_API_KEY, // required
+    verbose: false, // default: false
+    timeout: 100000, // default: 5000
+    https: true, // default: true
+    urlCount: 5, // default: 5
+    bufferSize: 8, // default: 8
+    unit: UNITS.Mbps // default: Bps
+});
+
+// Perform speed test, log result + push to Notion database.
+function performTest() {
+  speedtest.getSpeed().then(s => {
+      console.log(`Speed: ${s} Mbps`);
+      addItem(`Speed: ${s} Mbps at ${Date()}`)
+  }).catch(e => {
+      console.error(e.message);
+  });
+}
+// Run!
+performTest();
+
+// Run in 30 minute intervals.
+setInterval(performTest, 1800000); 
